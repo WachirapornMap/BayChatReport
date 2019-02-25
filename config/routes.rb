224bba1496b4227e5737217b504c5b13adabfdcc
Wachiraporn Mapatang <wachiraporn@amivoicethai.com>
2019-02-25 12:24:35 +0700

    add create job

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'report/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'report#index'
-   match 'search_product',to: 'report#search', via: :post
+  # match 'search_product',to: 'report#search', via: :post
    match 'export_report',to: 'report#export', via: :post
-   match 'show_json',to: 'report#show_json', via: :get
+   match 'create_job', to: 'report#createjob',via: :put
 end
